3f261a33a3630ec7b37ba4d7d454b3e0fbc802ad
Pradyot Prakash <pp2105@gmail.com>
2016-10-02 02:28:56 +0530

    feed forward nn

---
+++ mnist.py
@@ -0,0 +1,122 @@
+import tensorflow as tf
+from scipy import misc
+import glob, os
+import numpy as np
+
+def ffnn(inp, numLayers, weights, biases, activationFunction):
+	layer = inp
+
+	for i in xrange(1, numLayers):
+		wi = 'w' + str(i)
+		bi = 'b' + str(i)
+
+		p = tf.matmul(layer, weights[wi])
+		layer = activationFunction(tf.add(p, biases[bi]))
+
+	return layer
+
+
+def createParams(numLayers, widths):
+	weights = {}
+	biases = {}
+
+	for i in xrange(numLayers - 1):
+		wi = 'w' + str(i+1)
+		bi = 'b' + str(i+1)
+
+		weights[wi] = tf.Variable(tf.random_normal([widths[i], widths[i+1]]))
+		biases[bi] = tf.Variable(tf.random_normal([widths[i+1]]))
+
+	return weights, biases
+
+
+def readData(folder, inputD=None, outputD=None):
+	count = 0
+	images = []
+
+	os.chdir(folder)
+	print 'Starting reading up images from %s'%folder
+	for imfile in glob.glob('*.png'):
+		count += 1
+
+		if count % 100 == 0: print count
+		image = misc.imresize(misc.imread(imfile), [160, 160])
+		images.append(image.flatten())
+
+	images = np.asarray(images, dtype=np.float32)
+
+	labelsTemp = []
+	classes = set()
+	with open('labels.txt') as f:
+		for row in f:
+			cl = int(row)
+			classes.add(cl)
+			labelsTemp.append(cl)
+
+	if inputD is None:
+		inputD = images.shape[1]
+		outputD = len(classes)
+
+	labels = np.zeros(shape=(images.shape[0], outputD), dtype=np.float32)
+	for i in xrange(images.shape[0]):
+		labels[i, labelsTemp[i]] = 1.0
+
+	os.chdir('..')
+	print 'Images read from %s'%folder
+
+	del labelsTemp, classes
+
+	return images, labels, inputD, outputD
+
+
+def main(inputD, outputD, numLayers, widths, activationFunction, lossFunction, learningRate):
+	weights, biases = createParams(numLayers, widths)
+
+	x = tf.placeholder(tf.float32, [None, inputD])
+	y = ffnn(x, numLayers, weights, biases, activationFunction)
+	yOriginal = tf.placeholder(tf.float32, [None, outputD])
+
+	loss = tf.reduce_mean(-tf.reduce_sum(lossFunction(yOriginal, y), reduction_indices=[1]))
+	trainStep = tf.train.AdamOptimizer(learningRate).minimize(loss)
+
+	init = tf.initialize_all_variables()
+	sess = tf.Session()
+	sess.run(init)
+
+	batches = trainImages.shape[0]/100
+	for epoch in xrange(1000):
+		print 'Epoch: %s'%epoch
+		for i in xrange(batches):
+			# print 'Batch: %s'%i
+			xs = trainImages[100*i: 100*(i+1)]
+			ys = trainLabels[100*i: 100*(i+1)]
+			sess.run(trainStep, feed_dict={x: xs, yOriginal: ys})
+
+	correctPredication = tf.equal(tf.argmax(y, 1), tf.argmax(yOriginal, 1))
+	accuracy = tf.reduce_mean(tf.cast(correctPredication, tf.float32))
+
+	ret =  100.0*sess.run(accuracy, feed_dict={x: testImages, yOriginal: testLabels})
+	sess.close()
+
+	return ret
+
+if __name__ == '__main__':
+	# trainImages, trainLabels, inputD, outputD = readData('sampleTrain')
+	# testImages, testLabels, _, _ = readData('sampleValid', inputD, outputD)
+
+	from tensorflow.examples.tutorials.mnist import input_data
+	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+	trainImages, trainLabels, inputD, outputD = mnist.train.images, mnist.train.labels, mnist.train.images.shape[1], mnist.train.labels.shape[1]
+	testImages, testLabels = mnist.test.images, mnist.test.labels
+
+	crossEntropy = lambda yOrig, y: yOrig * tf.log(y + 1e-20)
+	l2Loss = lambda yOrig, y: tf.nn.l2_loss(yOrig - y)	
+
+	widths = [inputD, outputD]
+	numLayers = len(widths)
+	activationFunction = tf.nn.softmax # tf.tanh
+	lossFunction = crossEntropy
+	learningRate = 0.01
+
+	print main(inputD, outputD, numLayers, widths, activationFunction, lossFunction, learningRate)
+
